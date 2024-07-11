b076670c090ae2a7af148e1deb49fab66c4b60fe
evgeniyTagil <evgeniytagil.road@gmail.com>
2024-07-11 14:30:24 +0300

    Revert "Exercise"
    This reverts commit 321e69597afb17ca6c81764032f947ce052560b9.

--- src/main/java/ru/clevercec/check/sevice/DiscountCardService.java
+++
@@ -1,8 +0,0 @@
-package ru.clevercec.check.sevice;
-
-import ru.clevercec.check.model.DiscountCard;
-
-public interface DiscountCardService {
-
-    DiscountCard getDiscountCardByNumber(int number);
-}
--- src/main/java/ru/clevercec/check/sevice/ProductService.java
+++
@@ -1,8 +0,0 @@
-package ru.clevercec.check.sevice;
-
-import ru.clevercec.check.model.Product;
-
-public interface ProductService {
-
-    Product getProductById(int id);
-}
